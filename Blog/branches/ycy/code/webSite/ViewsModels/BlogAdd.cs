74e37e51ce5ea1dd713184ccf67e9cd72bc35e01
ycy0430 <1192724658@qq.com>
2020-10-02 23:50:50 +0800

    基本功能结束
    To：栏目博文的管理、删除

---
+++ Blog/branches/ycy/code/webSite/ViewsModels/BlogAdd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace web.ViewsModels
+{
+    public class BlogAdd
+    {
+        [Required]
+        [StringLength(250)]
+        public string Title { set; get; }
+        public string Content { set; get; }
+        public int catalogId { set; get; }
+    }
+}
