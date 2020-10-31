0caeea24d454d2bf25df0f6a96bc37891ad90568
ycy0430 <1192724658@qq.com>
2020-10-31 11:58:16 +0800

    1

--- Blog/trunk/code/webSite/Include/assets/waifu-tips.js
+++ Blog/trunk/code/webSite/Include/assets/waifu-tips.js
@@ -192,43 +192,43 @@ function initModel(waifuPath) {
     }
     loadModel(modelId, modelTexturesId);
 
-    $.ajax({
-        cache: true,
-        url: '/include/assets/waifu-tips.json',
-        dataType: "json",
-        success: function(result) {
-            $.each(result.mouseover, function(index, tips) {
-                $(document).on("mouseover", tips.selector, function() {
-                    var text = tips.text;
-                    if (Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1) - 1];
-                    text = text.render({ text: $(this).text() });
-                    showMessage(text, 3000);
-                });
-            });
-            $.each(result.click, function(index, tips) {
-                $(document).on("click", tips.selector, function() {
-                    var text = tips.text;
-                    if (Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1) - 1];
-                    text = text.render({ text: $(this).text() });
-                    showMessage(text, 3000, true);
-                });
-            });
-            $.each(result.seasons, function(index, tips) {
-                var now = new Date();
-                var after = tips.date.split('-')[0];
-                var before = tips.date.split('-')[1] || after;
+//    $.ajax({
+//        cache: true,
+//        url: '/include/assets/waifu-tips.json',
+//        dataType: "json",
+//        success: function(result) {
+//            $.each(result.mouseover, function(index, tips) {
+//                $(document).on("mouseover", tips.selector, function() {
+//                    var text = tips.text;
+//                    if (Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1) - 1];
+//                    text = text.render({ text: $(this).text() });
+//                    showMessage(text, 3000);
+//                });
+//            });
+//            $.each(result.click, function(index, tips) {
+//                $(document).on("click", tips.selector, function() {
+//                    var text = tips.text;
+//                    if (Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1) - 1];
+//                    text = text.render({ text: $(this).text() });
+//                    showMessage(text, 3000, true);
+//                });
+//            });
+//            $.each(result.seasons, function(index, tips) {
+//                var now = new Date();
+//                var after = tips.date.split('-')[0];
+//                var before = tips.date.split('-')[1] || after;
 
-                if ((after.split('/')[0] <= now.getMonth() + 1 && now.getMonth() + 1 <= before.split('/')[0]) &&
-                    (after.split('/')[1] <= now.getDate() && now.getDate() <= before.split('/')[1])) {
-                    var text = tips.text;
-                    if (Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1) - 1];
-                    text = text.render({ year: now.getFullYear() });
-                    showMessage(text, 6000, true);
-                }
-            });
-        }
-    });
-}
+//                if ((after.split('/')[0] <= now.getMonth() + 1 && now.getMonth() + 1 <= before.split('/')[0]) &&
+//                    (after.split('/')[1] <= now.getDate() && now.getDate() <= before.split('/')[1])) {
+//                    var text = tips.text;
+//                    if (Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1) - 1];
+//                    text = text.render({ year: now.getFullYear() });
+//                    showMessage(text, 6000, true);
+//                }
+//            });
+//        }
+//    });
+//}
 
 function loadModel(modelId, modelTexturesId) {
     localStorage.setItem('modelId', modelId);
